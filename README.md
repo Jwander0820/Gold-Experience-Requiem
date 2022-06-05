f32a6af29edd233b122ec22a63cb8551fcdc8598
Jwander0820 <66709644+Jwander0820@users.noreply.github.com>
2022-06-05 14:30:19 +0800

    This is Gold Experience Requiem!

---
+++ README.md
@@ -0,0 +1,4 @@
+# Gold Experience Requiem!
+**一個無用的圖像生成器**
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+class GoldExperienceRequiem:
+    print("This is Gold Experience Requiem!")
+
+
+if __name__ == '__main__':
+    GoldExperienceRequiem()
